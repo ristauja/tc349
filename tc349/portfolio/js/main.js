9d1b03c58755c1d57ed20553cccc2ea2be6c5c3f
ristauja <ristauja@msu.edu>
2014-11-10 19:51:45 -0500

    portfolio

---
+++ tc349/portfolio/js/main.js
@@ -0,0 +1,72 @@
+
+function OnPressImage1 ()
+{
+	var imageElement= document.getElementById("big picture");
+	imageElement.src= "img/image1.jpg";
+}
+
+function OnPressImage2 ()
+{
+	var imageElement= document.getElementById("big picture");
+	imageElement.src= "img/image2.jpg";
+}
+
+function OnPressImage3 ()
+{
+	var imageElement= document.getElementById("big picture");
+	imageElement.src= "img/image3.jpg";
+}
+
+function OnPressImage4 ()
+{
+	var imageElement= document.getElementById("big picture");
+	imageElement.src= "img/image4.jpg";
+}
+
+function OnPressImage5 ()
+{
+	var imageElement= document.getElementById("big picture");
+	imageElement.src= "img/image5.jpg";
+}
+
+function OnPressImage6 ()
+{
+	var imageElement= document.getElementById("big picture");
+	imageElement.src= "img/image6.jpg";
+}
+
+function OnPressImage7 ()
+{
+	var imageElement= document.getElementById("big picture");
+	imageElement.src= "img/image7.jpg";
+}
+
+function OnPressImage8 ()
+{
+	var imageElement= document.getElementById("big picture");
+	imageElement.src= "img/image8.jpg";
+}
+
+function OnPressImage9 ()
+{
+	var imageElement= document.getElementById("big picture");
+	imageElement.src= "img/image8.jpg";
+}
+
+function OnPressImage10 ()
+{
+	var imageElement= document.getElementById("big picture");
+	imageElement.src= "img/image10.jpg";
+}
+
+function OnPressImage11 ()
+{
+	var imageElement= document.getElementById("big picture");
+	imageElement.src= "img/image11.jpg";
+}
+
+function OnPressImage12 ()
+{
+	var imageElement= document.getElementById("big picture");
+	imageElement.src= "img/image12.jpg";
+}
