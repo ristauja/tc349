9eb0b8f9ae100369699eee8238c3b1c2cc6dfe88
ristauja <ristauja@msu.edu>
2014-12-09 18:51:11 -0500

    final project

---
+++ tc349/final/js/main.js
@@ -0,0 +1,30 @@
+
+function OnPressImage1 ()
+{
+  var imageElement= document.getElementById("big picture");
+  imageElement.src= "img/c9team.png";
+}
+
+function OnPressImage2 ()
+{
+  var imageElement= document.getElementById("big picture");
+  imageElement.src= "img/c91.jpg";
+}
+
+function OnPressImage3 ()
+{
+  var imageElement= document.getElementById("big picture");
+  imageElement.src= "img/c92.jpg";
+}
+
+function OnPressImage4 ()
+{
+  var imageElement= document.getElementById("big picture");
+  imageElement.src= "img/c93.jpg";
+}
+
+function OnPressImage5 ()
+{
+  var imageElement= document.getElementById("big picture");
+  imageElement.src= "img/c94.jpeg";
+}
